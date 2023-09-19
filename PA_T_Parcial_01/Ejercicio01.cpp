b0a3f96801dd5135b55c43bf8f2aed56379d318a
DavidLepe31 <d.alconerolepe@gmail.com>
2023-09-19 08:32:15 -0600

    hola

--- PA_T_Parcial_01/Ejercicio01.cpp
+++ PA_T_Parcial_01/Ejercicio01.cpp
@@ -1,5 +1,10 @@
 #include "Ejercicio01.h"
+#include<iostream>
+#include<ctype.h>
 
+using namespace std;
+int n=0;
+int m=0;
 char Ejercicio01::lower(char c)
 {
     if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
@@ -10,5 +15,17 @@ char Ejercicio01::lower(char c)
 
 bool Ejercicio01::isPalindrome(const char* phrase, int length)
 {
+    n = length-m;
+    string* p = new string[length];
+    for (int i = 0; i < length; i++) {
+        *(p + i) = *(phrase + i);
+    }
+    if (n == 1) {
+        return true;
+    }
+    
+    
+
+
     return false;
 }
--- PA_T_Parcial_01/Ejercicio02.cpp
+++ PA_T_Parcial_01/Ejercicio02.cpp
@@ -1,6 +1,14 @@
 #include "Ejercicio02.h"
-
+#include<iostream>
+#include<algorithm>
+using namespace std;
 int Ejercicio02::rob(int houses[], int n)
 {
-    return 0;
+    int dinero;
+    int* inicial = new int[n];
+    *(inicial + 0) = max(houses[0], houses[1]);
+    for (int i = 2; i <= n; i++) {
+        *(inicial + 0) += max(houses[i - 1], houses[i - 2]);
+    }
+    return *(inicial + 0);
 }
